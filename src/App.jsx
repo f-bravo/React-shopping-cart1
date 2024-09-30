05ec464beff02c9577bc05a5c7470f1b47d025db
f-bravo <bravoback@outlook.com>
2024-09-30 11:09:30 -0300

    Initial and Navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BsCart } from 'react-icons/bs'
+
+const Navbar = () => {
+  return (
+    <div className='d-flex justify-content-between bg-secondary py-3 px-5 text-white'>
+        <a href="" className='navbar-brand fs-4 fw-bolder'>Shop</a>
+        <a href="" className='navbar-link fs-5 text-white'><BsCart /></a>
+    </div>
+  )
+}
+
+export default Navbar
